133126212b74c5ae4db282960492cefba8e8a759
ioanamarinelababa1 <ioanamarinelababa1@gmail.com>
2026-04-12 19:53:47 +0300

    feat: generate tasks module, controller and service + add Task entity with status enum and TypeORM decorators
    This reverts commit 5dc89fcbdee5af38c361226dd261ad77187989c9.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -20,7 +19,6 @@ import { TasksModule } from './tasks/tasks.module';
       }),
       inject: [ConfigService],
     }),
-    TasksModule,
   ],
 })
 export class AppModule {}
