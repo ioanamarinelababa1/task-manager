67190565e790daa3be332c58ffb00b2a6c8cb8eb
ioanamarinelababa1 <ioanamarinelababa1@gmail.com>
2026-04-12 20:00:41 +0300

    feat: implement tasks CRUD - service, controller and module

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  findAll(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() body: Partial<Task>): Promise<Task> {
+    return this.tasksService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: Partial<Task>): Promise<Task> {
+    return this.tasksService.update(+id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.tasksService.remove(+id);
+  }
+}
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task, TaskStatus } from './task.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepository: Repository<Task>,
+  ) {}
+
+  findAll(): Promise<Task[]> {
+    return this.tasksRepository.find();
+  }
+
+  async findOne(id: number): Promise<Task> {
+    const task = await this.tasksRepository.findOne({ where: { id } });
+    if (!task) throw new NotFoundException(`Task #${id} not found`);
+    return task;
+  }
+
+  create(data: Partial<Task>): Promise<Task> {
+    const task = this.tasksRepository.create(data);
+    return this.tasksRepository.save(task);
+  }
+
+  async update(id: number, data: Partial<Task>): Promise<Task> {
+    await this.tasksRepository.update(id, data);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.tasksRepository.delete(id);
+  }
+}
